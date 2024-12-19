9ffed49c3d0f49f60423714067dd71a6677a7b79
kate0321vs <ekaterina.chechulina2103@gmail.com>
2024-12-19 15:01:53 +0600

    Создала состояние и вывела компоненты шарика и создала к ним стили

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import './App.css'
+import Ball from "./components/Ball/Ball.tsx";
+import {useState} from "react";
+
+interface IBall {
+    num: number;
+    id: number;
+}
 
 const App = () => {
+    const [balls, setBall] = useState<IBall[]>([
+        {num:5, id:1},
+        {num:11, id:2},
+        {num:16, id:3},
+        {num:23, id:4},
+        {num:32, id:5},
+    ]);
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+      <div className="ballsContainer">
+          {balls.map((ball) => (
+              <Ball key={ball.id} num={ball.num}/>
+          ))}
+      </div>
+  );
 };
 
 export default App
+
